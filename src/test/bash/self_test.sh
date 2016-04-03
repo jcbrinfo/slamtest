ce77d5717acc15d52553a07cf84eb4b4140b3f69
jcbrinfo <jcbrinfo@users.noreply.github.com>
2016-04-03 12:21:38 -0400

    self_test: Remove the temporary files of the previous run
    Signed-off-by: jcbrinfo <jcbrinfo@users.noreply.github.com>

--- src/test/bash/self_test.sh
+++ src/test/bash/self_test.sh
@@ -3,10 +3,13 @@
 
 run_test() {
 	local TMP_DIR=target/test/in
+	local test_name="${1##*/}"
+
+	rm -rf "${TMP_DIR}/${test_name}"
 	mkdir -p -- "${TMP_DIR}"
 	cp -a -t "${TMP_DIR}" -- "$1"
 	(
-		cd "${TMP_DIR}/${1##*/}"
+		cd "${TMP_DIR}/${test_name}"
 		set -- ../../../../target/main/slamtest
 		. main
 	) < "$1" > "$2" 2>&1
